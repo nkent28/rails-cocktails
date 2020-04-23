1db98c7b6e53e461cff754ebe23bb570782c0723
Nadia Kent nkent28@gmail.com <nkent28@gmail.com>
2020-04-24 00:49:07 +1200

    add

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_22_171848) do
+ActiveRecord::Schema.define(version: 2020_04_23_124416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,12 @@ ActiveRecord::Schema.define(version: 2020_04_22_171848) do
     t.string "description"
     t.integer "cocktail_id"
     t.integer "ingredient_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
